6a4e27a8471bd8890159665f04af876d4db51a52
PIPATPONG C <101804179+nomsodcaramel@users.noreply.github.com>
2023-04-19 15:24:16 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/mosaic.svg";
+import image from "../images/saturday.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,25 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Hi, I'm First I will become a university student this year at SIT KMUTT. Interest in AI, Data Science and New Technology. Active to learn and activities, I loved when I got vision or motivation from exploring new experiences and meeting people. If you want to know more about me or interest the same things, you can contact me from dm or email :)";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Artificial Intelligence",
+  "Data Science",
+  "Python",
+  "Leadership",
+  "Cognitive Flexibility",
+  "Creativity",
+  "Communication",
+  "Teamwork",
+  "Problem Solving",
+  "Critical Thinking",
+  "Time Management",
+  "Self-Learning",
 ];
 
 /**
@@ -45,8 +51,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+  "I want to sleep";
 const About = () => {
   return (
     <section className="light" id="about">
