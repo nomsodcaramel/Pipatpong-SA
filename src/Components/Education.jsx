220ece554e38d59aa99b56e76e6ff4586ab2ce7b
PIPATPONG C <101804179+nomsodcaramel@users.noreply.github.com>
2023-04-20 07:58:04 +0000

    modified:   src/Components/Education.jsx 	modified:   src/index.html

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -3,38 +3,27 @@ import React from "react";
 const Education = () => {
   return (
     <section className="light" id="education">
-      {/* grid of 4 education cells */}
+      {/* grid of 2 education cells */}
+      <h2>Education</h2>
       <div
+        className="container"
         style={{
-          backgroundColor: "white",
+          backgroundColor: "#D2F1E4",
           width: "50%",
           padding: "4rem",
-          margin: "3rem auto",
+          margin: "4rem auto",
           textAlign: "center",
         }}
       >
-        <h2>Education</h2>
-        <div className="container">
-          <div className="box">
-            <h3>University of California, Irvine</h3>
-            <p className="small">Bachelor of Science in Computer Science</p>
-            <p className="small">Expected Graduation: June 2022</p>
-          </div>
-          <div className="box">
-            <h3>University of California, Irvine</h3>
-            <p className="small">Bachelor of Arts in Cognitive Science</p>
-            <p className="small">Expected Graduation: June 2022</p>
-          </div>
-          <div className="box">
-            <h3>University of California, Irvine</h3>
-            <p className="small">Bachelor of Arts in Linguistics</p>
-            <p className="small">Expected Graduation: June 2022</p>
-          </div>
-          <div className="box">
-            <h3>University of California, Irvine</h3>
-            <p className="small">Bachelor of Arts in Cognitive Science</p>
-            <p className="small">Expected Graduation: June 2022</p>
-          </div>
+        <div className="box">
+          <h3>King mongkut’s university of technology thonburi</h3>
+          <p className="small">Bachelor of Art in Digital Service Innovation</p>
+          <p className="small">Become in this year</p>
+        </div>
+        <div className="box">
+          <h3>Chonkanyanukoon School</h3>
+          <p className="small">Major in Science-Computer</p>
+          <p className="small">Expected Graduation: February 2023</p>
         </div>
       </div>
     </section>
